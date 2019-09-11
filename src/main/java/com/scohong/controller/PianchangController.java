b20840d74e66ee220dda2aa0959fef8fc8f5f678
scohong <michaelhdb@qq.com>
2019-09-11 22:53:36 +0800

    添加节目和商家详情页的视频信息

--- src/main/java/com/scohong/controller/PianchangController.java
+++ src/main/java/com/scohong/controller/PianchangController.java
@@ -2,7 +2,8 @@ package com.scohong.controller;
 
 import com.scohong.Main;
 import com.scohong.dao.PianchangDao;
-import com.scohong.entity.pianchangDO.Movie;
+import com.scohong.entity.pianchangDO.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @Author: scohong
@@ -17,26 +19,37 @@ import java.io.IOException;
  * @Description:
  */
 @RestController
+@Slf4j
 public class PianchangController {
     @Autowired
     PianchangDao pianchangDao;
 
+
     @GetMapping("/data")
     public String getData() {
         //新版09-09
+        //添加场景详情placeDetail
         File[] files = new File("E:\\片场\\newMovies\\").listFiles();
-        for (File file : files) {
+        for (File file : files){
             try {
                 Movie movie = Main.pianchang2JsonToEntity(file);
-                System.out.println(movie.getCname()+ " " + movie.getType());
-                //添加新电影
-                pianchangDao.insertMovie(movie);
-                //添加新场景
-//                String movieName = movie2.getData().getMovie().getCname();
-//                List<Plot> plots = movie2.getData().getMovie().getPlots();
-//                for (Plot p:plots) {
-//                    pianchangDao.insertPlot(p,movieName);
-//                }
+//                System.out.println(movie.getCname()+ " " + movie.getType());
+                log.info(""+movie.getId());
+                List<PlaceDetail> placeDetails = movie.getPlaceDetails();
+                //获取palceDetail数据
+                if (placeDetails.size() > 0) {
+                    log.info("地点："+placeDetails.get(0).getCname());
+                    Scene scene = placeDetails.get(0).getScenes().get(0);
+                    //获取scene数据
+                    log.info("节目："+scene.getMovieCname());
+                    log.info("场景介绍："+scene.getSceneOverride());
+                    //detail
+                    Detail detail = scene.getDetails().get(0);
+                    log.info("details:" + detail.getDescription() );
+                    //场景描述，多图
+                    Still still = detail.getStills().get(0);
+                    log.info("still:" + still.getPicPath());
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- src/main/java/com/scohong/dao/PianchangDao.java
+++ src/main/java/com/scohong/dao/PianchangDao.java
@@ -24,4 +24,11 @@ public interface PianchangDao {
             "#{plot.upLevelAreaEname},#{plot.sceneId},#{plot.sceneName},#{plot.episode},#{plot.position},#{plot.coverPath})")
     int insertPlot( Plot plot, String movieName);
 
+    @Insert("insert into movie_place_relation(movie_name,place_id,place_cname,place_ename,lat,lng,ass_type,assed_id,assed_cname,assed_ename," +
+            "up_level_area_id,up_level_area_cname,up_level_area_ename,scene_id,scene_name,episode,`position`,cover_path) " +
+            "VALUES (#{movieName},#{plot.placeId},#{plot.placeCname},#{plot.placeEname},#{plot.lat},#{plot.lng},#{plot.assType}, " +
+            "#{plot.assedId},#{plot.assedCname},#{plot.assedEname},#{plot.upLevelAreaId},#{plot.upLevelAreaCname}, " +
+            "#{plot.upLevelAreaEname},#{plot.sceneId},#{plot.sceneName},#{plot.episode},#{plot.position},#{plot.coverPath})")
+    int insertPlotDetail( Plot plot, String movieName);
+
 }
